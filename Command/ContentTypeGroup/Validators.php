e2a9d73b341ee56208d61684034e9f621212da40
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-08-03 09:01:18 +0200

    add command/validators/service model

---
+++ Command/ContentTypeGroup/Validators.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emdro
+ * Date: 03/08/2016
+ * Time: 08:55
+ */
+
+namespace EdgarEz\ToolsBundle\Command\ContentTypeGroup;
+
+
+class Validators
+{
+
+}
---
+++ Service/ContentTypeGroup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emdro
+ * Date: 03/08/2016
+ * Time: 08:54
+ */
+
+namespace EdgarEz\ToolsBundle\Service;
+
+
+class ContentTypeGroup
+{
+
+}
