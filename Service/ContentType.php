ac36dea819487479a80f5b03df05b198ab53f1a0
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2016-08-04 16:10:53 +0200

    fix

--- Service/ContentType.php
+++ Service/ContentType.php
@@ -45,30 +45,32 @@ class ContentType
         $contentTypeCreateStruct = $contentTypeService->newContentTypeCreateStruct($struct['contentTypeIdentifier']);
         $contentTypeCreateStruct->mainLanguageCode = $struct['contentTypeMainLanguage'];
         $contentTypeCreateStruct->nameSchema = '<' . $struct['contentTypeNameSchema'] . '>';
+        $contentTypeCreateStruct->isContainer = (isset($struct['isContainer'])) ? $struct['isContainer'] : false;
         // set names for the content type
         $contentTypeCreateStruct->names = array(
             $struct['contentTypeMainLanguage'] => $struct['contentTypeName']
         );
 
-        for ($i = 0; $i < count($struct['attributes']); $i++) {
-            $attribute = $struct['attributes'][$i];
-            $fieldCreateStruct = $this->addAttribute($contentTypeService, $attribute, $i * 10);
+        $i = 1;
+        foreach ($struct['fields'] as $field) {
+            $fieldCreateStruct = $this->addField($contentTypeService, $field, $i * 10);
             $contentTypeCreateStruct->addFieldDefinition($fieldCreateStruct);
+            $i++;
         }
 
         $contentTypeDraft = $contentTypeService->createContentType($contentTypeCreateStruct, array($struct['contentTypeGroup']) );
         $contentTypeService->publishContentTypeDraft($contentTypeDraft);
     }
 
-    private function addAttribute(ContentTypeService $contentTypeService, array $attribute, $pos = 10)
+    private function addField(ContentTypeService $contentTypeService, array $field, $pos = 10)
     {
-        $fieldCreateStruct = $contentTypeService->newFieldDefinitionCreateStruct($attribute['identifier'], $attribute['type']);
-        $fieldCreateStruct->names = array($attribute['mainLanguage'] => $attribute['name']);
+        $fieldCreateStruct = $contentTypeService->newFieldDefinitionCreateStruct($field['identifier'], $field['type']);
+        $fieldCreateStruct->names = array($field['mainLanguage'] => $field['name']);
         $fieldCreateStruct->fieldGroup = 'content';
         $fieldCreateStruct->position = $pos;
-        $fieldCreateStruct->isTranslatable = $attribute['isTranslatable'];
-        $fieldCreateStruct->isRequired = $attribute['isRequired'];
-        $fieldCreateStruct->isSearchable = $attribute['isSearchable'];
+        $fieldCreateStruct->isTranslatable = (isset($field['isTranslatable'])) ? $field['isTranslatable'] : true;
+        $fieldCreateStruct->isRequired = (isset($field['isRequired'])) ? $field['isRequired'] : true;
+        $fieldCreateStruct->isSearchable = (isset($field['isSearchable'])) ? $field['isSearchable'] : true;
 
         return $fieldCreateStruct;
     }
